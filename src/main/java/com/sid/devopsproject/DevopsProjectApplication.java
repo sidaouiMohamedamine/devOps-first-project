ef8ba1074c2a081dd2c070c9daa525c08568975b
Sidaoui <mssidaoui@gmail.com>
2023-10-02 00:38:04 +0100

    add_dependency_and_code

--- src/main/java/com/sid/devopsproject/DevopsProjectApplication.java
+++ src/main/java/com/sid/devopsproject/DevopsProjectApplication.java
@@ -2,10 +2,14 @@ package com.sid.devopsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsProjectApplication {
-
+	@GetMapping
+	public String message() { return "welcome from sidaoui"; }
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsProjectApplication.class, args);
 	}
