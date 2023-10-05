9bce66a1f2a42547acc0ccb1aebda5625aeb17b6
Sidaoui <mssidaoui@gmail.com>
2023-10-05 03:10:08 +0100

    rename project_and_dockerfile_name

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:17-jdk
 EXPOSE 8080
-ADD target/devops-project-0.0.1-SNAPSHOT.jar devops-project.jar
-ENTRYPOINT ["java","-jar","devops-project.jar"]
+ADD target/devops-project-sid-0.0.1-SNAPSHOT.jar devops-project-sid.jar
+ENTRYPOINT ["java","-jar","devops-project-sid.jar"]
--- src/main/java/com/sid/devopsproject/DevopsProjectSidApplication.java
+++ src/main/java/com/sid/devopsproject/DevopsProjectSidApplication.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DevopsProjectApplication {
+public class DevopsProjectSidApplication {
 	@GetMapping
 	public String message() { return "welcome from sidaoui"; }
 	public static void main(String[] args) {
-		SpringApplication.run(DevopsProjectApplication.class, args);
+		SpringApplication.run(DevopsProjectSidApplication.class, args);
 	}
 
 }
